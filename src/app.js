566c12b0ab76d3ea49096719bfd50c0e6ec2f87d
Romzeer <romain.peyret1@gmail.com>
2018-03-30 17:58:44 +0200

    test

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ function processEvent(event) {
                             //console.log(path.extname(value.imageUrl));
 
                             if (value.imageUrl.includes(".gif") && !value.imageUrl.includes(PNG, JPG, JPEG)) {
-                               sendFBMessage(sender, {text: value.imageUrl});
+                               sendFBMessage(sender, {picture: value.imageUrl});
                             } else {
 
                                 //message.reply(Bot.Message.picture(value.imageUrl));
