8365fdca0d90159e3b9ae91a8aaad6a8e9196c71
Romzeer <romain.peyret1@gmail.com>
2018-03-30 16:13:45 +0200

    test

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function processEvent(event) {
             });
 
         apiaiRequest.on('response', (response) => {
-            //(response);
+            console.log(response);
             if (isDefined(response.result)) {
                 let responseText = response.result.fulfillment.speech;
                 let responseData = response.result.fulfillment.data;
