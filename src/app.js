25f089c58ce7eb6cde590446c59ccf063a6fd4f7
Romzeer <romain.peyret1@gmail.com>
2018-03-30 18:27:34 +0200

    test

--- src/app.js
+++ src/app.js
@@ -76,20 +76,7 @@ function processEvent(event) {
                             //console.log(path.extname(value.imageUrl));
                             
                             if (value.imageUrl.includes(".gif") && !value.imageUrl.includes(PNG, JPG, JPEG)) {
-                                var attachment = {
-                                    'type':'template',
-                                    'payload':{
-                                        'template_type':'generic',
-                                        'elements':[
-                                            {
-                                                'title':'Chocolate Cookie',
-                                                'image_url': value.imageUrl,
-                                                'subtitle':'A delicious chocolate cookie',
-                                            },
-                                        ]
-                                    }
-                                };
-                               sendFBMessage(sender, {text: "test", attachement: attachment});
+                              sendGenericMessage(sender);
                             } else {
 
                                 //message.reply(Bot.Message.picture(value.imageUrl));
@@ -222,6 +209,55 @@ function sendFBSenderAction(sender, action, callback) {
             }
         });
     }, 1000);
+}
+
+function sendGenericMessage(sender) {
+    let messageData = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "First card",
+				    "subtitle": "Element #1 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "https://www.messenger.com",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
+				    }],
+			    }, {
+				    "title": "Second card",
+				    "subtitle": "Element #2 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+				    "buttons": [{
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for second element in a generic bubble",
+				    }],
+			    }]
+		    }
+	    }
+    }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json: {
+		    recipient: {id:sender},
+		    message: messageData,
+	    }
+    }, function(error, response, body) {
+	    if (error) {
+		    console.log('Error sending messages: ', error)
+	    } else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
 }
 
 function doSubscribeRequest() {
