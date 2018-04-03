bb2dfcc15fbb6f2deeb5d0ceb7a12ee731fd3ea9
Romzeer <romain.peyret1@gmail.com>
2018-04-03 12:09:35 +0200

    test

--- src/app.js
+++ src/app.js
@@ -207,7 +207,7 @@ function sendFBMessage(sender, messageData, callback) {
             message: messageData
         }
     }, (error, response, body) => {
-        console.log(body);
+        console.log("body:" + body);
         if (error) {
             console.log('Error sending message: ', error);
         } else if (response.body.error) {
