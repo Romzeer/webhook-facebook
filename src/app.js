4ef5167c023d8fc57b72984bd8780864f624c26a
Romzeer <romain.peyret1@gmail.com>
2018-04-04 16:53:57 +0200

    test

--- src/app.js
+++ src/app.js
@@ -101,7 +101,7 @@ function processEvent(event) {
                     steps.forEach(step => {
                         console.log("before change: " + step.oldEquation.ascii());  // e.g. before change: 2x + 3x = 35
                         console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
-                        console.log("after change: " + step.newEquation.asscii());   // e.g. after change: 5x = 35
+                        console.log("after change: " + step.newEquation.ascii());   // e.g. after change: 5x = 35
                         console.log("# of substeps: " + step.substeps.length);    // e.g. # of substeps: 2
                         let beforeChange = { text: "before change: " + step.oldEquation.ascii()};  // e.g. before change: 2x + 3x = 35
                         let change = { text: "change: " + step.changeType};                  // e.g. change: SIMPLIFY_LEFT_SIDE
