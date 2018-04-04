c633d1f8f74771fc63d1c560546045b482955519
Romzeer <romain.peyret1@gmail.com>
2018-04-04 16:42:38 +0200

    test mathsteps

--- src/app.js
+++ src/app.js
@@ -97,16 +97,19 @@ function processEvent(event) {
                       secondMessage = "";
                 }
                 if (action == "input.calcul") {
-                    let steps = mathsteps.solveEquation(text);
+                    let steps = mathsteps.simplifyExpression(text);
                     steps.forEach(step => {
-                      let beforeChange = { text: "before change: " + step.oldEquation.ascii()};  // e.g. before change: 2x + 3x = 35
-                      let change = { text: "change: " + step.changeType};                  // e.g. change: SIMPLIFY_LEFT_SIDE
-                      let afterChange = { text: "after change: " + step.newEquation.ascii()};   // e.g. after change: 5x = 35
-                      let substeps = { text: "# of substeps: " + step.substeps.length};      // e.g. # of substeps: 2
-                        
-                      messagesDatas.push(beforeChange, change, afterChange, substeps);
-                      sendFBMessage(sender, messagesDatas, 0);
+                        console.log("before change: " + step.oldEquation.ascii());  // e.g. before change: 2x + 3x = 35
+                        console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
+                        console.log("after change: " + step.newEquation.ascii());   // e.g. after change: 5x = 35
+                        console.log("# of substeps: " + step.substeps.length);    // e.g. # of substeps: 2
+                        let beforeChange = { text: "before change: " + step.oldEquation.ascii()};  // e.g. before change: 2x + 3x = 35
+                        let change = { text: "change: " + step.changeType};                  // e.g. change: SIMPLIFY_LEFT_SIDE
+                        let afterChange = { text: "after change: " + step.newEquation.ascii()};   // e.g. after change: 5x = 35
+                        let substeps = { text: "# of substeps: " + step.substeps.length};   
                     });
+                    
+                   
                 }
                 
                 else if (isDefined(messages) && messages.length > 1) {
